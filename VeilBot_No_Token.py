69c9c679630b8d0de51c7fed48a5b507f7f06b51
Keaten Holley <30886216+Keatenh@users.noreply.github.com>
2018-01-08 13:15:07 -0600

    added additional commands including simple web scraping to give game stats in real time

--- VeilBot_No_Token.py
+++ VeilBot_No_Token.py
@@ -5,6 +5,26 @@ import asyncio
 import time
 import random #random library
 
+# ~WS~
+from urllib.request import urlopen as uReq #grab the page itself
+from bs4 import BeautifulSoup as soup #parse html text
+
+my_url = 'http://steamcharts.com/app/381210#48h'
+
+# opening up connection, grabbing the page
+uClient = uReq(my_url)
+# off load content into a variable
+page_html = uClient.read()
+# close client
+uClient.close()
+
+# html parsing
+page_soup = soup(page_html, "html.parser")
+
+# grab info we want
+pop_game = page_soup.findAll("span",{"class":"num"})
+time_game = page_soup.findAll("abbr",{"class":"timeago"})
+
 Client = discord.Client()
 client = commands.Bot(command_prefix = "!")
 
@@ -31,6 +51,16 @@ Stage = ["Coal Tower",
 "Mother's Dwelling",
 "Badham Preschool"]
 
+Players = ["campers",
+"Absolute Idiots",
+"Meatbags",
+"Salty Individuals",
+"Horror Fiends",
+"Michael Fangirls",
+"Self Loathers",
+"Entity Worshipers",
+"Dead Hard Users",
+"BBQ Users"] 
 @client.event
 async def on_ready():
 	print("Bot is ready!")
@@ -39,7 +69,7 @@ async def on_ready():
 async def on_message(message):
 	if message.content.upper().startswith('!COMMANDS'):
 		userID = message.author.id
-		await client.send_message(message.channel, "try !wraith , !q , !mbs")
+		await client.send_message(message.channel, "try !wraith , !q , !mbs, !players")
 	if message.content.upper().startswith('!WRAITH'):
 		userID = message.author.id
 		await client.send_message(message.channel, "<@%s> bing-BONG!" % (userID))
@@ -62,6 +92,17 @@ async def on_message(message):
 		stageNum = random.randint(0,21)
 		stageTxt = str(Stage[stageNum])
 		await client.send_message(message.channel, (stageTxt) +" "+ "is the MOST Billy Stage."+ " " + "<@%s>" % (userID))
+	if message.content.upper().startswith('!PLAYERS'):
+		userID = message.author.id
+		population = pop_game[0].text.strip() #removes white space
+		popTxt = str(population)
+		playersNum = random.randint(0,len(Players))
+		playersTxt = str(Players[playersNum])
+		time = time_game[0].text.strip()
+		timeTxt = str(time)
+		await client.send_message(message.channel,"There are currently " +(popTxt) + " " + (playersTxt) + " " + "playing DBD. " + "<@%s>" % (userID))
+		#await client.send_message(message.channel,"There are " +(popTxt) + " " + (playersTxt) + " " + "playing DBD as of " + (timeTxt) + ". " + "<@%s>" % (userID))
+
 		
 client.run("INSERT TOKEN HERE") #token from DC/devs/apps
 
